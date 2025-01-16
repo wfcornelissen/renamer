e3c5296ce4ff91cc0cd9dfc0038db3ba3ce2cac0
wfcornelissen <wfcornelissen@gmail.com>
2025-01-16 12:03:16 +0200

    Reading and entries done. Next is selection of manual entry or autonumber.

--- main.go
+++ main.go
@@ -3,30 +3,75 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	/*files := []string{}
+	files := []string{}
 	filepath.WalkDir("/mnt/f/Scans", func(path string, d fs.DirEntry, err error) error {
 		files = append(files, path)
 		return nil
 	})
-	fmt.Println(files)*/
+
+	for _, file := range files {
+		fmt.Println(file)
+	}
+	// Print all company names with numbers
 	for i, company := range companies {
 		fmt.Println(i, company.name)
 	}
 
+	// Get all inputs
+	companySelection, startingNumber, lcOrExternalPOD, invoiceNumber := getAllInputs()
+
+	// Create the document name
+	docName := fmt.Sprintf("%sPOD%d_LC%d_INV%d", companies[companySelection].fileStart, startingNumber, lcOrExternalPOD, invoiceNumber)
+	fmt.Println(docName) //debug
+
+	// Confirm the document name - debugging
+	if userConfirmation(docName) {
+		fmt.Println("Confirmed")
+	} else {
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func getAllInputs() (int, int, int, int) {
+	fmt.Println("Please enter the company number: ")
+	companySelection := getInput()
+	fmt.Println("Please enter internal POD number: ")
+	startingNumberSelection := getInput()
+	fmt.Println("Please enter the Loadcon or external POD number: ")
+	lcOrExternalPOD := getInput()
+	fmt.Println("Please enter the invoice number: ")
+	invoiceNumber := getInput()
+	return companySelection, startingNumberSelection, lcOrExternalPOD, invoiceNumber
+}
+
+func getInput() int {
 	scan := bufio.NewScanner(os.Stdin)
 	scan.Scan()
 	input, err := strconv.Atoi(scan.Text())
 	if err != nil {
-		fmt.Println("Invalid input")
+		fmt.Println("Invalid input: Unknown command")
 		os.Exit(1)
 	}
-	fmt.Println(companies[input].name)
+	return input
+}
 
-	os.Exit(0)
+func userConfirmation(userInput string) bool {
+	fmt.Println("Please confirm the input: ", userInput)
+	scan := bufio.NewScanner(os.Stdin)
+	scan.Scan()
+	input := scan.Text()
+	if strings.ToLower(input) == "y" || strings.ToLower(input) == "yes" {
+		return true
+	}
+	return false
 }
--- structures.go
+++ structures.go
@@ -9,36 +9,30 @@ type company struct {
 var no1Online = company{
 	name:      "Number 1 Online Solutions",
 	fileStart: "/N1O_",
-	fileNum:   0,
 }
 
 var VirtigoResources = company{
 	name:      "Virtigo Resources",
 	fileStart: "/VirRes_",
-	fileNum:   0,
 }
 var VirtigoAgri = company{
 	name:      "Virtigo Agri",
 	fileStart: "/VirAgri_",
-	fileNum:   0,
 }
 
 var VirtigoSandAndStone = company{
 	name:      "Virtigo Sand And Stone",
 	fileStart: "/VirSand_",
-	fileNum:   0,
 }
 
 var LutzvilleCellars = company{
 	name:      "Lutzville Cellars",
 	fileStart: "/LCell_",
-	fileNum:   0,
 }
 
 var NamakwaLogistics = company{
 	name:      "Namakwa Logistics",
 	fileStart: "/NamLog_",
-	fileNum:   0,
 }
 
 var companies = []company{
